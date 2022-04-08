e43162b9a9d7747c6cc79f18cd1d446483d35d1c
laorpaksa9000 <jaydee.laorpaksa@rocdeleijgraaf.nl>
2022-04-08 09:29:44 +0200

    lijst

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 
     </head>
     <body>
-       <?php
-        echo "dit is een test bestand"; 
-       ?>
+	<legend>Registratie</legend>
+	Naam: <input name="naam" type="tekst"><br>
+    Adres: <input name="adres" type="tekst"><br>
+    Postcode:<input name="postcode" type="tekst"><br>
+	Woonplaats: <input name="woonplaats" type="tekst"><br>
+	Emailadress:<input name="emailadress" type="tekst"><br>
     </body>
 </html>
