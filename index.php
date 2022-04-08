05d7dd1e0758f35d59f1e240f1b5b0f5c4a1c3ef
Daan de Klein <0085940@rocdeleijgraaf.nl>
2022-04-08 09:42:27 +0200

    form en knop toegevoegd

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
     </head>
     <body>
 	<legend>Registratie</legend>
-	Naam: <input name="naam" type="tekst"><br>
-    Adres: <input name="adres" type="tekst"><br>
-    Postcode:<input name="postcode" type="tekst"><br>
-	Woonplaats: <input name="woonplaats" type="tekst"><br>
-	Emailadress:<input name="emailadress" type="tekst"><br>
+    <form action="/verhuur.php" method="$_POST">
+        Naam: <input name="naam" type="tekst"><br>
+        Adres: <input name="adres" type="tekst"><br>
+        Postcode:<input name="postcode" type="tekst"><br>
+        Woonplaats: <input name="woonplaats" type="tekst"><br>
+        Emailadress:<input name="emailadress" type="tekst"><br>
+        <input type="submit" value="submit">
+    </form>
     </body>
 </html>
