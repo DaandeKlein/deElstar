8e89a459ca018e2d5015b563756bafb2467494d9
Daan de Klein <0085940@rocdeleijgraaf.nl>
2022-04-13 11:50:10 +0200

    post verder uitgebreid en ik ga morgen de database linken aan de post

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 
     </head>
     <body>
-        <?php 
-            include("connect.php")
-        ?>
 	<legend>Registratie</legend>
     <form action="verhuur.php" method="$_POST">
         Naam: <input name="naam" type="tekst"><br>
@@ -13,7 +10,19 @@
         Postcode:<input name="postcode" type="tekst"><br>
         Woonplaats: <input name="woonplaats" type="tekst"><br>
         Emailadress:<input name="emailadress" type="tekst"><br>
-        <input type="submit" value="submit">
+        <input type="submit" value="submit" name="submit">
     </form>
+        <?php
+            include("connect.php");
+             
+            if (isset($_POST["submit"]))
+            {
+                $naam = $_POST["naam"];
+                $adres = $_POST["adres"];
+                $postcode = $_POST["postcode"];
+                $woonplaats = $_POST["woonplaats"];
+                $emailadress = $_POST["emailadress"];
+            }
+        ?>
     </body>
 </html>
