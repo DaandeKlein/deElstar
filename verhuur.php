62d06f59e8a8d80697c88520922e99d2a344d802
Daan de Klein <0085940@rocdeleijgraaf.nl>
2022-04-20 09:58:55 +0200

    data van de database komt op de verhuur pagina

--- verhuur.php
+++ verhuur.php
@@ -17,12 +17,31 @@
 
                 $sql = "INSERT INTO klant(Naam, Adres, Woonplaats, Telefoon, Email) 
                 VALUES (?, ?, ?, ?, ?);";
-                $pdo->prepare($sql)->execute([$naam, $adres, $woonplaats, $telefoon, $emailadress]);                
+                $pdo->prepare($sql)->execute([$naam, $adres, $woonplaats, $telefoon, $emailadress]);
             }
 
+            unset($_POST["submit"]);
 
         ?>
 
+        <?php 
+        
+            $query = "SELECT * FROM fiets";
+            $query_run = mysqli_query($conn, $query);
+            $check_fiets = mysqli_num_rows($query_run) > 0;
+
+            if($check_fiets)
+            {
+                while($row = mysqli_fetch_array($query_run))
+                {
+                   echo $row['Type'];
+                }
+            }
+            else
+            {
+                echo "No elstar items found";
+            }
+        ?>
         <div class="fietsen">
             <p>Type</p>
             <p>Merk</p>
