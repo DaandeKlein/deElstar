bca91ec6c6e972a9effad2a46d35cfcfa9ef840e
Valentino Thoonen <valentino.thoonen@rocdeleijgraaf.nl>
2022-04-21 09:34:44 +0200

    Gemaakt

---
+++ dank.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+    <title> De Elstar </title>
+		<meta charset=utf-8>
+		<meta name=description content="beschrijving">
+		<meta name=keywords content="trefword, trefword">
+		<link rel="stylesheet" href="dank.css">
+    </head>
+    <body>
+    
+        <p>Bedankt voor het huren bij fietsverhuur de Elstar.</p>
+        <a href="index.php" class="link_button">Home</a>
+
+
+
+    </body>
+
+
+
+
+
+
+
+
+
+</html>
