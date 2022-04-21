c5e9236eef87515be4bc21775a776ab28236c452
laorpaksa9000 <jaydee.laorpaksa@rocdeleijgraaf.nl>
2022-04-21 09:28:13 +0200

    y e s

--- index.php
+++ index.php
@@ -11,17 +11,43 @@
 	<div class ="center">
     <legend class = "legend">Registratie</legend>
     <form action="verhuur.php" method="post">
-    <label class ="labels">Naam:</label>
-    <input class ="inputlabel" name="Naam" type="tekst"><br>
-    <label class ="labels2">Adres:</label>
-        <input class ="inputlabel2" name="Adres" type="tekst"><br>
-        <label class ="labels3">Woonplaats:</label>
-        <input class ="inputlabel3" name="Woonplaats" type="tekst"><br>
-        <label class ="labels4">Telefoon:</label>
-        <input class ="inputlabel4" name="Telefoon" type="tekst"><br>
-        <label class ="labels5">Emailadress:</label>
-        <input class ="inputlabel5" name="emailadress" type="tekst"><br>
-        <input class ="submit" type="submit" value="submit" name="submit">
+    
+<table>
+    <tbody>
+    <tr><td><label class ="labels">Naam:</label></td>
+    <td><input class ="inputlabel" name="Naam" type="tekst"></td></tr>
+</tbody>
+</table>
+
+<table>
+    <tbody>
+    <tr><td><label class ="labels2">Adres:</label></td>
+    <td><input class ="inputlabel2" name="Adres" type="tekst"></td></tr>
+</tbody>
+</table>
+
+<table>
+    <tbody>
+    <tr><td><label class ="labels3">Woonplaats:</label></td>
+    <td><input class ="inputlabel3" name="Woonplaats" type="tekst"><br></td></tr>
+    </tbody>
+</table>
+
+<table>
+    <tbody>
+    <tr><td><label class ="labels4">Telefoon:</label></td>
+    <td><input class ="inputlabel4" name="Telefoon" type="tekst"><br></td></tr>
+    </tbody>
+</table>
+
+<table>
+    <tbody>
+    <tr><td><label class ="labels5">Emailadress:</label></td>
+    <td><input class ="inputlabel5" name="emailadress" type="tekst"><br></td></tr>
+    </tbody>
+</table>
+
+    <input class ="submit" type="submit" value="submit" name="submit">
         </form>    
         </div> 
         </div>    
